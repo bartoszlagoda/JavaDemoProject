e399b679784fabedb7259f84a727b9a4862c344e
bartosz.lagoda <composit095@gmail.com>
2023-12-24 13:11:50 +0100

    MetodyTest classes with types updated once again

--- src/Lekcje/Metody/Metody.java
+++ src/Lekcje/Metody/Metody.java
@@ -24,4 +24,30 @@ public class Metody {
         // pozwala ono na zwrocenie wartosci z metody (zgodnej z zadeklarowanym typem metody)
         return result;
     }
+
+    public void policzWynikParam(int number) { // wewnatrz nawiasu podajemy wartosc, ktora ma byc dostarczana przy wywolaniu metody
+        System.out.println("Number ma wartosc: " + number);
+        System.out.println("Zaraz policze wynik");
+        int result = 0;
+        for (int i = 0; i < number; i++) { // tutaj chcemy uzyc argumentu funkcji
+            result = result + i;
+        }
+        System.out.println("Rezultat to: " + result);
+    }
+
+    public void add(int firstNumber, int secondNumber) {
+        // po wcisnietym ALT + ENTER mamy utworzona metode z mozliwoscia konfigurowania jej
+        System.out.println("Suma to: " + (firstNumber + secondNumber));
+    }
+    // w argumentach funkcji można podawać argumenty różnych typów, nie tylko tego, który jest zwracany
+    public int addInteger(int firstNumber, int secondNumber) {
+        // po wcisnietym ALT + ENTER mamy utworzona metode z mozliwoscia konfigurowania jej
+//        System.out.println("Suma to: " + (firstNumber + secondNumber));
+//        if (firstNumber == 0) {
+//            return 0; // to jest sciezka TRUE, ale musi byc jeszcze FALSE
+//        }
+        // to jest sciezka FALSE
+        return firstNumber + secondNumber;
+        // po return już nic nie wywołamy
+    }
 }
--- src/Lekcje/Metody/Test/MetodyTest.java
+++ src/Lekcje/Metody/Test/MetodyTest.java
@@ -5,14 +5,35 @@ import Lekcje.Metody.Metody;
 public class MetodyTest {
     public static void main(String[] args) {
         Metody metody = new Metody();
-        System.out.println("Wywołanie metody typu void:");
-        metody.policzWynik();
-        // int result = metody.policzWynik();
-        // metoda typu void nie określa typu zwracanej wartosci
-        // nie da się takiej metody przypisać do żadnej zmiennej
-        System.out.println("Rezultat metody typu int pobierzWynik to:");
-        metody.pobierzWynik();
-        int rezultat = metody.pobierzWynik() * 2; // zwróci wszystko z metody
-        System.out.println("Rezultat metody typu int pobierzWynik pomnozonej razy 2 to: \n" + rezultat); // zwroci 9900
+
+//        System.out.println("Wywołanie metody typu void:");
+//        metody.policzWynik();
+//        // int result = metody.policzWynik();
+//        // metoda typu void nie określa typu zwracanej wartosci
+//        // nie da się takiej metody przypisać do żadnej zmiennej
+//
+//        System.out.println("Rezultat metody typu int pobierzWynik to:");
+//        metody.pobierzWynik();
+//        int rezultat = metody.pobierzWynik() * 2; // zwróci wszystko z metody
+//
+//        System.out.println("Rezultat metody typu int pobierzWynik pomnozonej razy 2 to: \n" + rezultat); // zwroci 9900
+//
+//        System.out.println("Rezultat metody z podanym argumentem funkcji 200:  ");
+//        metody.policzWynikParam(200);
+//
+//        System.out.println("Rezultat metody z podanym argumentem funkcji 500:  ");
+//        metody.policzWynikParam(500);
+//
+//        System.out.println("Rezultat metody z podanym argumentem funkcji 1000:  ");
+//        metody.policzWynikParam(1000);
+////        metody.policzWynikParam("String tu nie zadziala ! Musi byc int!");
+//        metody.add(2,3); // ALT + ENTER utworzy nam nieistniejaca jeszcze metody przy tak wpisanych wartosciach
+
+        System.out.println("Rezultat z metody dodajacej argumenty zwracajacej INT");
+        // metody.addInteger(457,891);
+        //System.out.println("Wypisz mi wartosc z metody addInteger: " + metody.addInteger(457,891));
+        System.out.println(metody.addInteger(457,891));
+        int result = metody.addInteger(8,5);
+        System.out.println(result);
     }
 }
