0beab04f977b6a7fd62b2637aec2855e1e6cb81d
bartosz.lagoda <composit095@gmail.com>
2023-12-24 11:59:18 +0100

    StudentTest class created

---
+++ src/Lekcje/Klasy/Test/StudentTest.java
@@ -0,0 +1,41 @@
+package Lekcje.Klasy.Test;
+
+import Lekcje.Klasy.Student;
+
+// Stworz 3 obiekty klasy Student. Przypisz wartosci do pol. Stworz tablice i dodaj studentow do tablicy.
+// Przejdz przez studentow w tablicy. Wywolaj wszystkie cztery metody.
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.imie = "Jan";
+        student1.nazwisko = "Kowalski";
+        student1.email = "jan.kowalski@student.uczelnia.pl";
+        student1.nick = "jan.kowalski";
+        student1.numerIndeksu = 311212;
+
+        Student student2 = new Student();
+        student2.imie = "Janusz";
+        student2.nazwisko = "Nowak";
+        student2.email = "janusz.nowak@student.uczelnia.pl";
+        student2.nick = "janusz.nowak";
+        student2.numerIndeksu = 311213;
+
+        Student student3 = new Student();
+        student3.imie = "Janina";
+        student3.nazwisko = "Kozlowska";
+        student3.email = "janina.kozlowska@student.uczelnia.pl";
+        student3.nick = "janina.kozlowska";
+        student3.numerIndeksu = 311214;
+
+        Student[] studenci = new Student[] {student1,student2,student3};
+
+        for (int i = 0; i < studenci.length; i++) {
+            System.out.println("Dane studenta nr " + (i + 1));
+            studenci[i].przedstawSie();
+            studenci[i].podajNrIndeksu();
+            studenci[i].podajEmail();
+            studenci[i].zalogujSie();
+            System.out.println("----------------------------");
+        }
+    }
+}
