5e43374677910b60b3c3c647d3c755f7138cafbb
bartosz.lagoda <composit095@gmail.com>
2023-12-24 12:02:45 +0100

    LekcjaTestowa class created

---
+++ src/Lekcje/LekcjaTestowa.java
@@ -0,0 +1,9 @@
+package Lekcje;
+
+public class LekcjaTestowa {
+    // psvm -> skrot do uruchomienia ponizszej metody
+    public static void main(String[] args) {
+        // sout -> skrot do uruchomienia ponizszej metody
+        System.out.println("Witaj Bartosz!");
+    }
+}
