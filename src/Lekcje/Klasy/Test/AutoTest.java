30c1edf22d80c49d8f7c63a397b9ef8255782fdc
bartosz.lagoda <composit095@gmail.com>
2023-12-27 15:33:18 +0100

    Auto,AutoTest task upgraded

--- src/Lekcje/Klasy/Test/AutoTest.java
+++ src/Lekcje/Klasy/Test/AutoTest.java
@@ -6,12 +6,13 @@ public class AutoTest {
     public static void main(String[] args) {
 
         // deklaracja zmiennej typu Auto (tworzenie obiektu klasy Auto)
-        Auto mercedes = new Auto();
+        Auto mercedes = new Auto("Mercedes","Klasa S",2021,1000);
+//        Auto mercedes = new Auto();
         // przypisanie wartosci do pol obiektu mercedes
-        mercedes.marka = "Mercedes";
-        mercedes.model = "Klasa S";
-        mercedes.rokProdukcji = 2021;
-        mercedes.przebieg = 1000;
+//        mercedes.marka = "Mercedes";
+//        mercedes.model = "Klasa S";
+//        mercedes.rokProdukcji = 2021;
+//        mercedes.przebieg = 1000;
 
         // wywołanie metod klasy Auto na obiekcie mercedes
         mercedes.jedz(); // CTRL + TAB -> powrót do początkowej klasy
@@ -19,11 +20,12 @@ public class AutoTest {
         mercedes.info();
 
         // deklaracja nowej zmiennej typu Auto
-        Auto audi = new Auto();
-        audi.marka = "Audi";
-        audi.model = "A5";
-        audi.rokProdukcji = 2020;
-        audi.przebieg = 0;
+//        Auto audi = new Auto();
+        Auto audi = new Auto("Audi","A5",2020,0);
+//        audi.marka = "Audi";
+//        audi.model = "A5";
+//        audi.rokProdukcji = 2020;
+//        audi.przebieg = 0;
 
         // wywołanie metod klasy Auto na obiekcie audi
         audi.jedz();
