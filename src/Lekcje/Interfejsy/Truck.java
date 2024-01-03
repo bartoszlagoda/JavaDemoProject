30f7b4fbe077c9a800ac3b92590ee9b70431ebc1
bartoszlagoda <composit095@gmail.com>
2024-01-03 11:50:26 +0100

    Interface lesson completed

---
+++ src/Lekcje/Interfejsy/Truck.java
@@ -0,0 +1,18 @@
+package Lekcje.Interfejsy;
+
+public class Truck implements Vehicle{
+    @Override
+    public void jazda(int speed) {
+        System.out.println("Jade ciezarowka z predkoscia " + speed);
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Hamuje ciezarowka!");
+    }
+
+    @Override
+    public String info() {
+        return "Ciezarowka";
+    }
+}
