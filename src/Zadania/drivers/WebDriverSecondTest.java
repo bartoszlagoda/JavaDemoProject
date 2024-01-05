25bf21a0b25ced28c1a6aef382e0c202792bd192
bartosz.lagoda <composit095@gmail.com>
2024-01-05 15:55:34 +0100

    JAVA-132: Anonymus class lesson completed

--- src/Zadania/drivers/WebDriverSecondTest.java
+++ src/Zadania/drivers/WebDriverSecondTest.java
@@ -1,5 +1,31 @@
 package Zadania.drivers;
 
 public class WebDriverSecondTest {
+    public static void main(String[] args) {
+        WebDriver chrome = new ChromeDriver();
+        WebDriver secondChrome = new ChromeDriver();
 
+        WebDriver firefox = new FirefoxDriver();
+        WebDriver secondFirefox = new FirefoxDriver();
+        WebDriver thirdFirefox = new FirefoxDriver();
+
+        // klasa anonimowa
+        WebDriver safari = new WebDriver() {
+            @Override
+            public String get() {
+                return "Otwieram stronę w Safari";
+            }
+
+            @Override
+            public String findElementBy() {
+                return "Znalazłem element w przegladarce Safari";
+            }
+            // proba ponownego stworzenia klasy anonimowej to ponowne tworzenie całego body
+        };
+
+        System.out.println(safari.get());
+        System.out.println(safari.findElementBy());
+
+
+    }
 }
