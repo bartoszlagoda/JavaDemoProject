cf910982f090ea227159d4fe25c8d9bf086142a5
bartosz.lagoda <composit095@gmail.com>
2024-01-05 18:23:53 +0100

    JAVA-133: Introduction to lambda

---
+++ src/Lekcje/Lambda/Student.java
@@ -0,0 +1,8 @@
+package Lekcje.Lambda;
+
+@FunctionalInterface
+public interface Student {
+
+    // jedyna mozliwa metoda w takim interfejsie
+    void sayHello(String name);
+}
---
+++ src/Lekcje/Lambda/StudentTest.java
@@ -0,0 +1,12 @@
+package Lekcje.Lambda;
+
+public class StudentTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void sayHello(String name, Student student){
+        student.sayHello(name);
+    }
+}
